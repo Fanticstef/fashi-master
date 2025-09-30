2eca248e52432fc1a2deb01cd6678d4b31a0564f
Sumit Kumar <ris1472@gmail.com>
2025-10-01 00:55:43 +0530

    same change

--- src/config/api/axiosInstance.js
+++ src/config/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
-    // Remove default Content-Type header to allow dynamic setting for file uploads
+    timeout: 10000,
 });
 
 // add token then axios Interceptor
