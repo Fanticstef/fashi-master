2a636eb872ea8287577fdb028ef3eea155b15fa8
Sumit Kumar <ris1472@gmail.com>
2025-10-01 21:53:38 +0530

    correct

--- src/config/api/axiosInstance.js
+++ src/config/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: 'https://fashi-master-backend.vercel.app',
     timeout: 10000,
 });
 
